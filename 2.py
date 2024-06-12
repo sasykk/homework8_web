0ae10fbcd89099ac97b24eeec832c481f2ddd031
stanislavsynyasvskyy <153124369+sasykk@users.noreply.github.com>
2024-06-12 23:29:53 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,1 +1,12 @@
+import json
+from models import Author
+from mongoengine import connect
 
+connect(host="mongodb+srv://sasykk:<password>@cluster0.gtx4smv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+
+with open('authors.json', 'r', encoding='utf-8') as file:
+    authors_data = json.load(file)
+
+for author_data in authors_data:
+    author = Author(**author_data)
+    author.save()
