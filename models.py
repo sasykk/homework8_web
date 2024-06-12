e3465dc09b3166c60f04f23b83daecd031e3ef0a
stanislavsynyasvskyy <153124369+sasykk@users.noreply.github.com>
2024-06-12 23:39:34 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,1 +1,12 @@
+from mongoengine import Document, StringField, ListField, ReferenceField
 
+class Author(Document):
+    fullname = StringField(required=True)
+    born_date = StringField(required=True)
+    born_location = StringField(required=True)
+    description = StringField()
+
+class Quote(Document):
+    tags = ListField(StringField())
+    author = ReferenceField(Author, required=True)
+    quote = StringField(required=True)
