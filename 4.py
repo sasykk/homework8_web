75521747fdea8ae2e054161c97b5238209c3958e
stanislavsynyasvskyy <153124369+sasykk@users.noreply.github.com>
2024-06-12 23:38:16 +0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,1 +1,33 @@
+import redis
+from mongoengine import connect
+from models import Author, Quote
 
+redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+connect(host="mongodb+srv://sasykk:<password>@cluster0.gtx4smv.mongodb.net/?retryWrites=true&w=majority")
+
+def search_quotes(query):
+    if redis_client.exists(query):
+        return redis_client.get(query)
+    command, value = query.split(':')
+    if command == 'name':
+        author = Author.objects(fullname__icontains=value).first()
+        if author:
+            quotes = Quote.objects(author=author)
+            result = quotes.to_json()
+            redis_client.setex(query, 60, result)
+            return result
+    elif command == 'tag' or command == 'tags':
+        tags = value.split(',')
+        quotes = Quote.objects(tags__in=tags)
+        result = quotes.to_json()
+        redis_client.setex(query, 60, result)
+        return result
+    return 'No quotes found.'
+
+while True:
+    query = input('Enter search command (name:author_name, tag:tag_name, tags:tag1,tag2): ')
+    if query.lower() == 'exit':
+        break
+    result = search_quotes(query)
+    print(result)
